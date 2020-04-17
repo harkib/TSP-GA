4397f8d58d2a6d7f42621439c273454b88f25083
harkib <harkib@sfu.ca>
2020-04-16 21:18:23 -0700

    NWOK, rw variations

--- main.py
+++ main.py
@@ -108,6 +108,7 @@ def GA_rw_pmx(fname, max_iter, prev_pop):
 
         #mutate childern
         next_gen = single_mutation(next_gen)
+        next_gen[-1] = curr_gen[0][1]
 
         #score childern
         assert len(next_gen) == pop_size 
@@ -116,13 +117,149 @@ def GA_rw_pmx(fname, max_iter, prev_pop):
 
     return curr_gen, scores
 
-def GA_rw_pmx(fname, max_iter, prev_pop):
+def GA_sq_pmx(fname, max_iter, prev_pop):
     pop_size = len(prev_pop)
     city_locs = tsp.load_city_locs(fname)
     curr_gen = prev_pop
     
     scores = []
-    print(f'rw_pmx("{fname}", max_iter={max_iter}, pop_size={pop_size}) ...')
+    print(f'rq_pmx("{fname}", max_iter={max_iter}, pop_size={pop_size}) ...')
+    for i in range(max_iter):
+
+        #record historic scores
+        scores.append(curr_gen[0][0])
+
+        #select parents
+        parents = selection_sq(curr_gen)
+
+        #gen childern -> next_gen
+        next_gen = []
+        for pair in parents:
+            #print(pair)
+            first, second = tsp.pmx(pair[0], pair[1])
+            next_gen.append(first)
+            next_gen.append(second)
+
+        #mutate childern
+        next_gen = single_mutation(next_gen)
+        next_gen[-1] = curr_gen[0][1]
+
+        #score childern
+        assert len(next_gen) == pop_size 
+        curr_gen = [(tsp.total_dist(p, city_locs), p) for p in next_gen]
+        curr_gen.sort()
+
+    return curr_gen, scores
+
+def GA_top_pmx(fname, max_iter, prev_pop):
+    pop_size = len(prev_pop)
+    city_locs = tsp.load_city_locs(fname)
+    curr_gen = prev_pop
+    
+    scores = []
+    print(f'top_pmx("{fname}", max_iter={max_iter}, pop_size={pop_size}) ...')
+    for i in range(max_iter):
+
+        #record historic scores
+        scores.append(curr_gen[0][0])
+
+        #select parents
+        parents = selection_top(curr_gen)
+
+        #gen childern -> next_gen
+        next_gen = []
+        for pair in parents:
+            #print(pair)
+            first, second = tsp.pmx(pair[0], pair[1])
+            next_gen.append(first)
+            next_gen.append(second)
+
+        #mutate childern
+        next_gen = single_mutation(next_gen)
+        next_gen[-1] = curr_gen[0][1]
+
+        #score childern
+        assert len(next_gen) == pop_size 
+        curr_gen = [(tsp.total_dist(p, city_locs), p) for p in next_gen]
+        curr_gen.sort()
+
+    return curr_gen, scores
+
+def GA_top_NWOX(fname, max_iter, prev_pop):
+    pop_size = len(prev_pop)
+    city_locs = tsp.load_city_locs(fname)
+    curr_gen = prev_pop
+    
+    scores = []
+    print(f'top_NWOX("{fname}", max_iter={max_iter}, pop_size={pop_size}) ...')
+    for i in range(max_iter):
+
+        #record historic scores
+        scores.append(curr_gen[0][0])
+
+        #select parents
+        parents = selection_top(curr_gen)
+
+        #gen childern -> next_gen
+        next_gen = []
+        for pair in parents:
+            #print(pair)
+            first, second = crossover_NWOX(pair)
+            next_gen.append(first)
+            next_gen.append(second)
+
+        #mutate childern
+        next_gen = single_mutation(next_gen)
+        next_gen[-1] = curr_gen[0][1]
+
+        #score childern
+        assert len(next_gen) == pop_size 
+        curr_gen = [(tsp.total_dist(p, city_locs), p) for p in next_gen]
+        curr_gen.sort()
+
+    return curr_gen, scores
+
+def GA_sq_NWOX(fname, max_iter, prev_pop):
+    pop_size = len(prev_pop)
+    city_locs = tsp.load_city_locs(fname)
+    curr_gen = prev_pop
+    
+    scores = []
+    print(f'sq_NWOX("{fname}", max_iter={max_iter}, pop_size={pop_size}) ...')
+    for i in range(max_iter):
+
+        #record historic scores
+        scores.append(curr_gen[0][0])
+
+        #select parents
+        parents = selection_sq(curr_gen)
+
+        #gen childern -> next_gen
+        next_gen = []
+        for pair in parents:
+            #print(pair)
+            first, second = crossover_NWOX(pair)
+            next_gen.append(first)
+            next_gen.append(second)
+
+        #mutate childern
+        next_gen = single_mutation(next_gen)
+        next_gen[-1] = curr_gen[0][1]
+
+        #score childern
+        assert len(next_gen) == pop_size 
+        curr_gen = [(tsp.total_dist(p, city_locs), p) for p in next_gen]
+        curr_gen.sort()
+
+    return curr_gen, scores
+
+def GA_rw_NWOX(fname, max_iter, prev_pop):
+    pop_size = len(prev_pop)
+    city_locs = tsp.load_city_locs(fname)
+    curr_gen = prev_pop
+    
+    scores = []
+    print(f'rw_nwox("{fname}", max_iter={max_iter}, pop_size={pop_size}) ...')
     for i in range(max_iter):
 
         #record historic scores
@@ -135,12 +272,13 @@ def GA_rw_pmx(fname, max_iter, prev_pop):
         next_gen = []
         for pair in parents:
             #print(pair)
-            first, second = tsp.pmx(pair[0], pair[1])
+            first, second = crossover_NWOX(pair)
             next_gen.append(first)
             next_gen.append(second)
 
         #mutate childern
         next_gen = single_mutation(next_gen)
+        next_gen[-1] = curr_gen[0][1]
 
         #score childern
         assert len(next_gen) == pop_size 
@@ -148,7 +286,7 @@ def GA_rw_pmx(fname, max_iter, prev_pop):
         curr_gen.sort()
 
     return curr_gen, scores
-    
+
 # uses roulette wheel selection, returns n/2 pairs of parents
 def selection_rw(curr_gen):
 
@@ -209,6 +347,59 @@ def selection_top(curr_gen):
 
     return parents[:math.ceil(n/2)]
 
+def crossover_NWOX(pair):
+    #print(pair)
+    p1 = pair[0]
+    p2 = pair[1]
+    c1 = []
+    c2 = []
+
+    n = len(p1)
+    a = random.randint(0,n-1)
+    b = random.randint(a,n-1)
+    #print("ab; ", a,b)
+
+    c1_mid = [elem for elem in p1[a:b] if elem not in p2[a:b]]
+
+    c1_left = [elem for elem in p1[:a] if elem not in p2[a:b]]
+    c1_lslide = c1_mid[:a-len(c1_left)]
+
+    # print(c1_left,len(c1_left))
+    # print(c1_lslide,len(c1_lslide))
+
+    c1_right =[elem for elem in p1[b:] if elem not in p2[a:b]]
+    c1_rslide = [elem for elem in c1_mid if elem not in c1_lslide]
+
+    # print(c1_right,len(c1_right))  
+    # print(c1_rslide,len(c1_rslide))
+
+    
+    # print(p2[a:b],len(p2[a:b]))
+    c1.extend(c1_left+c1_lslide+p2[a:b]+c1_rslide+c1_right)
+    # print(c1,len(c1))
+
+    # print("-----c2------")
+
+    c2_mid = [elem for elem in p2[a:b] if elem not in p1[a:b]]
+
+    c2_left = [elem for elem in p2[:a] if elem not in p1[a:b]]
+    c2_lslide = c2_mid[:a-len(c2_left)]
+
+    # print(c2_left,len(c2_left))
+    # print(c2_lslide,len(c2_lslide))
+
+    c2_right =[elem for elem in p2[b:] if elem not in p1[a:b]]
+    c2_rslide = [elem for elem in c2_mid if elem not in c2_lslide]
+
+    # print(c2_right,len(c1_right))
+    # print(c2_rslide,len(c2_rslide))
+
+    # print(p1[a:b],len(p1[a:b]))
+    c2.extend(c2_left+c2_lslide+p1[a:b]+c2_rslide+c2_right)
+    # print(c2,len(c2))
+
+    return c1,c2
+
 #returns mutated pop
 def single_mutation(next_gen):
     for lst in next_gen:
@@ -246,16 +437,34 @@ def save_pop(fname):
     # f_w.close()
     return []
 
-def test_all(fname, max_iter, prev_pop):
-    fin_pop_pmx, scores_pmx = GA_standard_pmx(fname, max_iter, prev_pop)
+def test_all(fname, max_iter, prev_pop): 
+
+    fin_pop_rw_nwox, scores_rw_nwox = GA_rw_NWOX(fname, max_iter, prev_pop)
+    plt.plot(scores_rw_nwox, label = "RW NWOX")
+
+    fin_pop_sq_nwox, scores_sq_nwox = GA_sq_NWOX(fname, max_iter, prev_pop)
+    plt.plot(scores_sq_nwox, label = "SQ NWOX")
+
+    fin_pop_top_nwox, scores_top_nwox = GA_top_NWOX(fname, max_iter, prev_pop)
+    plt.plot(scores_top_nwox, label = "top NWOX")
+
+    fin_pop_rw_pmx, scores_rw_pmx = GA_rw_pmx(fname, max_iter, prev_pop)
+    plt.plot(scores_rw_pmx, label = "RW PMX")
+
+    fin_pop_sq_pmx, scores_sq_pmx = GA_sq_pmx(fname, max_iter, prev_pop)
+    plt.plot(scores_sq_pmx, label = "SQ PMX")
+
+    fin_pop_top_pmx, scores_top_pmx = GA_top_pmx(fname, max_iter, prev_pop)
+    plt.plot(scores_top_pmx, label = "Top PMX")
+
     # fin_pop_cs, scores_cs = GA_crossover_search(fname, max_iter, prev_pop)
-    # fin_pop_ms, scores_ms = GA_mutate_search(fname, max_iter, prev_pop)
-
     # plt.plot(scores_cs, label = "crossover search")
-    # plt.plot(scores_ms, label = "mutation search")
-    plt.plot(scores_pmx, label = "standard with PMX")
 
-    plt.ylabel('Score')
+    fin_pop_ms, scores_ms = GA_mutate_search(fname, max_iter, prev_pop)
+    plt.plot(scores_ms, label = "mutation search")
+    
+
+    plt.ylabel('Score' + fname)
     plt.xlabel("Generation")
     plt.legend()
     plt.show()
@@ -275,7 +484,7 @@ def gen_rand_pop(fname, n_pop):
 if __name__ == '__main__':
 
     
-    n_pop = 100
+    n_pop = 20
     max_iter = 1000
     citesFname = "cities20.txt"
 
